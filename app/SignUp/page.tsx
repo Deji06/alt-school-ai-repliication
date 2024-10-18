43b25751f08837239bf28d37734f96d23ceab872
Deji06 <olawunioladeji06@gmail.com>
2024-10-18 14:31:22 +0100

    sign up auth

--- app/SignUp/page.tsx
+++ app/SignUp/page.tsx
@@ -3,17 +3,30 @@ import React from "react";
 import {useState} from 'react'
 import { FcGoogle } from "react-icons/fc";
 import Link from 'next/link'
+import {useCreateUserWithEmailAndPassword} from 'react-firebase-hooks/auth'
+import auth from '@/app/firebase/config'
 
 type Props = {};
 
 const SignUp = () => {
   // states
-  const [name, setName] = useState('')
+  // const [name, setName] = useState('')
   const[email, setEmail]= useState('');
   const[password, setPassword]= useState('');  
 
-  const handleForm = (e:any) => {
-    e.preventDefault()
+  const [createUserWithEmailAndPassword] = useCreateUserWithEmailAndPassword(auth)
+  
+  const handleSignUp = async () => {
+    try {
+      const resp = await createUserWithEmailAndPassword(email, password)
+      console.log('user response:' , resp);
+      setEmail('')
+      setPassword('')
+    } catch (error) {
+      console.error('error during sign up:',error);
+      
+      
+    }
   }
 
   return (
@@ -31,11 +44,11 @@ const SignUp = () => {
           <FcGoogle /> sign up with Google
         </a>
         <p className="mt-5 capitalize">or sign up with email</p>
-        <form action="" className="flex flex-col gap-y-5 mt-5" onSubmit={handleForm}>
-          <div className="flex flex-col gap-y-2">
+        <form action="" className="flex flex-col gap-y-5 mt-5">
+          {/* <div className="flex flex-col gap-y-2">
             <label htmlFor="">Full Name*</label>
             <input type="text" value={name} onChange={(e) => setName(e.target.value)}  placeholder="Deji Olawuni" className="border py-2 rounded-[10px] px-2"/>
-          </div>
+          </div> */}
           <div className="flex flex-col gap-y-2" >
             <label htmlFor="">Email*</label>
             <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}  placeholder="example@gmail.com" className="border py-2 rounded-[10px] px-2"/>
@@ -44,12 +57,12 @@ const SignUp = () => {
             <label htmlFor="">Password*</label>
             <input type="text"  value={password} onChange={(e) => setPassword(e.target.value)} placeholder="************" className="border py-2 rounded-[10px] px-2"/>
           </div>
-          <div className="flex flex-col gap-y-2">
+          {/* <div className="flex flex-col gap-y-2">
             <label htmlFor="">Confirm Password*</label>
             <input type="text"  placeholder="*******" className="border py-2 rounded-[10px] px-2"/>
-          </div>
+          </div> */}
         </form>
-        <button type="submit"  className="text-white bg-[#2F80ED] px-5 py-2 rounded-[10px] mt-5">sign up</button>
+        <button type="submit"  className="text-white bg-[#2F80ED] px-5 py-2 rounded-[10px] mt-5" onClick={handleSignUp}>sign up</button>
         <div className="flex items-center gap-x-1 mt-2">
           <Link href='/LogIn' className="underline">Login</Link>
           <p>if you already have an account</p>
@@ -62,3 +75,13 @@ const SignUp = () => {
 };
 
 export default SignUp;
+
+
+// const handleSignUp = async (email, password) => {
+  //   try {
+  //     const userCredential = await signInWithEmailAndPassword(auth, email, password);
+  //     console.log('User signed in:', userCredential.user);
+  //   } catch (error) {
+  //     console.error('Error signing in:', error);
+  //   }
+  // };
